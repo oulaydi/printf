d6099a67bdaa71e82bd7fb035dc30f74569d0ec3
oulaydi <oussama.oulaydi@gmail.com>
2023-10-14 17:13:30 +0100

    This is the _printf function

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
+ * _printf - this is a formatted output conversion and print data.
  * @format: input string.
  *
  * Return: number of chars printed.
